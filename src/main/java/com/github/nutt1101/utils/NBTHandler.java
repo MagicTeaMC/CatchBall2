d43cb8ed7ec00cf9ab980843cbb99018a6f5fda0
MagicTeaMC <oneowo87@gmail.com>
2025-05-22 19:27:49 +0800

    make it build

--- src/main/java/com/github/nutt1101/utils/NBTHandler.java
+++ src/main/java/com/github/nutt1101/utils/NBTHandler.java
@@ -44,6 +44,7 @@ public class NBTHandler {
                         ageableEntity.setAdult();
                     } else {
                         ageableEntity.setBaby();
+                    }
                 }
             }
         } catch (Exception e) {
