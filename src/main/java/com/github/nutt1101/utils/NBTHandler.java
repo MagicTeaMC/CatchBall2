67e25a23df1544bd0775442ffc02042b8465db59
Maoyue 貓月 <95519633+MagicTeaMC@users.noreply.github.com>
2025-05-22 06:27:06 +0000

    respect isBaby tag

--- src/main/java/com/github/nutt1101/utils/NBTHandler.java
+++ src/main/java/com/github/nutt1101/utils/NBTHandler.java
@@ -3,6 +3,7 @@ package com.github.nutt1101.utils;
 import de.tr7zw.changeme.nbtapi.NBTContainer;
 import de.tr7zw.changeme.nbtapi.NBTEntity;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
@@ -37,6 +38,13 @@ public class NBTHandler {
                 NBTContainer nbtContainer = new NBTContainer(nbtString);
                 NBTEntity nbtEntity = new NBTEntity(entity);
                 nbtEntity.mergeCompound(nbtContainer);
+
+                if (entity instanceof Ageable ageableEntity) {
+                    if (!nbtContainer.hasTag("IsBaby") || !nbtContainer.getBoolean("IsBaby")) {
+                        ageableEntity.setAdult();
+                    } else {
+                        ageableEntity.setBaby();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
