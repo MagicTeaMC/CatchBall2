7c7b9d010264a06fa51130889fd060e4589ef1f9
MagicTeaMC <oneowo87@gmail.com>
2025-06-09 19:35:10 +0800

    make it sort player name

--- src/main/java/com/github/nutt1101/command/TabComplete.java
+++ src/main/java/com/github/nutt1101/command/TabComplete.java
@@ -33,10 +33,10 @@ public class TabComplete implements TabCompleter {
             if (args.length == 2) { 
                 entityList.clear();
                 if (args[0].equalsIgnoreCase("give")) {
-                    Bukkit.getOnlinePlayers().forEach(player -> {
-                        sort.add(player.getName());
-                    });
-                    StringUtil.copyPartialMatches(args[0], sort, sort);
+                    List<String> playerNames = new ArrayList<>();
+                    Bukkit.getOnlinePlayers().forEach(player -> playerNames.add(player.getName()));
+                    StringUtil.copyPartialMatches(args[1], playerNames, sort);
+                    sort.sort(String.CASE_INSENSITIVE_ORDER);
                     return sort;
                 } else if (args[0].equalsIgnoreCase("add")) {
                     Set<String> allEntityList = ConfigSetting.entityFile.getConfigurationSection("EntityList").getKeys(false);
