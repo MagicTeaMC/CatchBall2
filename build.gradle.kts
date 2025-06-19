eef817271013d5619820502702975756137490ca
MagicTeaMC <oneowo87@gmail.com>
2025-06-19 19:10:44 +0800

    update nbt-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ repositories {
 dependencies {
     api("org.bstats:bstats-bukkit:3.1.0")
     api("com.jeff_media:SpigotUpdateChecker:3.0.4")
-    api("de.tr7zw:item-nbt-api:2.15.0")
+    api("de.tr7zw:item-nbt-api:2.15.1-20250619.003022-5")
     api("cn.handyplus.lib.adapter:FoliaLib:1.2.0")
     compileOnly("org.spigotmc:spigot-api:1.20.5-R0.1-SNAPSHOT")
     compileOnly("io.lumine:Mythic-Dist:5.9.0")
